7edb631b3c44ad5129c12c940fd990fc4893d3b2
yahyathegreat <yahyamardan1234@gmail.com>
2025-01-19 15:02:38 +0500

    solid and hollow

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ while not done:
             done = True
     pygame.draw.rect(screen, (0, 125, 225), pygame.rect(30, 30, 60, 60))
     pygame.display.flip()
+import pygame
+pygame.init()
+window = pygame.display.set_mode((400, 400))
+window.fill((255, 255, 255))
+GREEN = (0, 255, 0)
+pygame.draw.circle(window, GREEN, (300, 300), 50)
+pygame.draw.circle(window, GREEN, (100, 100), 550, 3)
+pygame.display.update()
+running =  True
+while running:
+    for event in pygame.get():
+        if event.type == pygame.QUIT:
+             running = False
+pygame.QUIT
